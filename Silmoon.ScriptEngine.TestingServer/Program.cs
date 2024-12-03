ff8aeb8f9ed682cc9c8f67a9b0b6bacbcdf06b3f
WEIBIN SONG <sone@silmoon.com>
2024-12-04 01:24:33 +0800

    fix test project file path.

--- Silmoon.ScriptEngine.TestingServer/Program.cs
+++ Silmoon.ScriptEngine.TestingServer/Program.cs
@@ -12,10 +12,19 @@ builder.Services.AddHostedService(p => p.GetService<EngineService>());
 
 builder.Services.Configure<EngineServiceOptions>(options =>
 {
-    options.ScriptFiles.Add(@"../../../../AuthTradingScripts/ScriptProgram.cs");
-    options.ScriptFiles.Add(@"../../../../AuthTradingScripts/EAScript1.cs");
-
-    options.ReferrerAssemblyPaths.Add(@"../../../../AutoTradingFrameworks/bin/Debug/net8.0/AutoTradingFrameworks.dll");
+    // if current working directory path include bin string:
+    if (Directory.GetCurrentDirectory().Contains("bin"))
+    {
+        options.ScriptFiles.Add(@"../../../../AuthTradingScripts/ScriptProgram.cs");
+        options.ScriptFiles.Add(@"../../../../AuthTradingScripts/EAScript1.cs");
+        options.ReferrerAssemblyPaths.Add(@"../../../../AutoTradingFrameworks/bin/Debug/net8.0/AutoTradingFrameworks.dll");
+    }
+    else
+    {
+        options.ScriptFiles.Add(@"../AuthTradingScripts/ScriptProgram.cs");
+        options.ScriptFiles.Add(@"../AuthTradingScripts/EAScript1.cs");
+        options.ReferrerAssemblyPaths.Add(@"../AutoTradingFrameworks/bin/Debug/net8.0/AutoTradingFrameworks.dll");
+    }
 
     options.ReferrerAssemblyNames.Add("System.Console");
     options.ReferrerAssemblyNames.Add("System.Runtime");
